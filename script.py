ccb68c1506b2b90f70eacdd5b0901be9287b7b57
platypus0417 <109408428+platypus0417@users.noreply.github.com>
2022-07-16 16:23:05 +0900

    Add files via upload

---
+++ script.py
@@ -0,0 +1,39 @@
+import webbrowser
+
+# HTML 수정 함수
+def write(name, desc):
+  Element(name).element.innerText = desc
+
+# 하단 버튼 링크 연결 함수
+def button(*args):
+  link = "https://www.nasa.gov/" # https:// 꼭 붙여야 연결됩니다!
+  webbrowser.open(link)
+
+# 배경 색깔 설정 함수
+def background(color):
+  for i in range(0, 2):
+    write("g" + str(i), color[i])
+
+def information(info):
+  key = list(info.keys())
+  for i in range(0, 4):
+    write("a" + str(i), key[i])
+    write("b" + str(i), info[key[i]])
+
+# 배경 색깔 설정
+colors = ["blue", "red"]
+background(colors)
+
+# 이름과 설명, 버튼에 들어갈 글 설정
+write("name", "이은택")
+write("description", "부산진중 2학년")
+write("button", "NASA")
+
+# 상세설명에 들어갈 제목과 글 설정
+informations = {
+  "싫어하는 음식": "오이",
+  "관심분야": "우주,소프트웨어",
+  "좋아하는 가수": "아이유",
+  "취미": "다큐 시청,테트리스"
+}
+information(informations)
